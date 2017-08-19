c21060705d2450fb692753e8e5079aba9104bf4a
vanida <lala.demon69@gmail.com>
2017-08-19 07:48:40 +0700

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <v-header />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import VHeader from './components/Header';
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    VHeader,
+  },
 }
 </script>
 
 <style>
+@import '../node_modules/normalize.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="root">
+    <div class="slideshow">
+      <ul class='slide'>
+        <li class="slide__item">
+          <img class="slide__img" src="../assets/pic_2.jpg" />
+          <span class="slide__text">Kitchen Cabinet</span>
+        </li>
+       </ul>
+      </div>
+   <div class="primary-menu">
+      <ul class="menu">
+        <li class="menu__item">
+          <router-link class="menu__link" to="/">Home</router-link>
+        </li>
+        <li class="menu__item">
+          <router-link class="menu__link" to="/about">About Us</router-link>
+        </li>
+        <li class="menu__item">
+          <router-link class="menu__link" to="/pages">Pages</router-link>
+        </li>
+        <li class="menu__item">
+          <router-link class="menu__link" to="/addons">Addons</router-link>
+        </li>
+        <li class="menu__item">
+          <router-link class="menu__link" to="/blog">Blog</router-link>
+        </li>
+        <li class="menu__item">
+          <router-link class="menu__link" to="/portfolio">Portfolio</router-link>
+        </li>
+        <li class="menu__item">
+          <router-link class="menu__link" to="/contact">Contact</router-link>
+        </li>
+      </ul> 
+   </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'template',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.slide {
+  width: 100%;
+  height: 600px;
+  object-fit: cover;
+}
+
+.primary-menu {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+}
+
+.root {
+  position: relative;
+}
+
+.menu {
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  padding: 0; 
+}
+
+.menu__item {
+  font-weight: bold; 
+  color: #ffffff;
+  margin: 1em;
+}
+
+.menu__link {
+  color: #ffffff; 
+  text-decoration: none;
+}
+
+.menu__link:hover {
+  color: #CC0000;
+  
+}
+
+</style>
